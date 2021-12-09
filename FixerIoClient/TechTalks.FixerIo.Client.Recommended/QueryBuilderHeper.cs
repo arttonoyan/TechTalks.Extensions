50634966f0be0a2d19e84675d118c88f8e8d83e4
Artyom Tonoyan <artonoyan@servicetitan.com>
2021-12-09 14:37:41 +0400

    Add QueryBuilderHeper

---
+++ FixerIoClient/TechTalks.FixerIo.Client.Recommended/QueryBuilderHeper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTalks.FixerIo.Client.Recommended
+{
+    public static class QueryBuilderHeper
+    {
+        public static string BuildQuery(params KeyValuePair<string, string>[] requestParams)
+        {
+            if (requestParams != null && requestParams.Length > 0)
+            {
+                var builder = new StringBuilder();
+                foreach (var (key, value) in requestParams)
+                {
+                    builder.Append(key).Append('=').Append(value).Append('&');
+                }
+                return builder.ToString().TrimEnd('&');
+            }
+            return "";
+        }
+    }
+}
